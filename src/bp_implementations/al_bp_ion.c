3f2ea97f565323116b6fe49dc1654cd90658d620
annettaws <anna.damico.ws@gmail.com>
2013-03-28 11:06:08 +0000

    Bug strtok

--- src/bp_implementations/al_bp_ion.c
+++ src/bp_implementations/al_bp_ion.c
@@ -297,11 +297,11 @@ al_bp_error_t bp_ion_recv(al_bp_handle_t handle,
 		printf("tmp_eid:%s\n",tmp_eid);
 		strtok(tmp_eid, "/");
 		printf("tmp_eid:%s\n",tmp_eid);
-		strtok(NULL, "/");
+		strtok(tmp_eid, "/");
 		printf("tmp_eid:%s\n",tmp_eid);
-		strtok(NULL, "/");
+		strtok(tmp_eid, "/");
 		printf("tmp_eid:%s\n",tmp_eid);
-		strtok(NULL, "/");
+		strtok(tmp_eid, "/");
 		printf("tmp_eid:%s\n",tmp_eid);
 		/* tmp_eid = "dtn://vm1.dtn/src_2222"
 		 * after
