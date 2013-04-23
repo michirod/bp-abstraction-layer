5213913abcf89957183ce32b4c31d621c229dc77
annettaws <anna.damico.ws@gmail.com>
2013-04-23 12:23:55 +0100

    TEST

--- src/bp_implementations/al_bp_ion.c
+++ src/bp_implementations/al_bp_ion.c
@@ -108,8 +108,9 @@ al_bp_error_t bp_ion_build_local_eid(al_bp_endpoint_id_t* local_eid,
 		}
 		long int service_num = strtol(service_tag,NULL,10);
 		printf("\nAL_BP: %lu\n",service_num);
-		sprintf(eidString, "%lu",
-								service_num);
+		printf("\nAL_BP: %lu\n",getOwnNodeNbr());
+		sprintf(eidString, "%s:%lu.%lu",
+								CBHESCHEMENAME,getOwnNodeNbr(),service_num);
 		printf("\nAL_BP: %s\n",eidString);
 		(*local_eid) = ion_al_endpoint_id(eidString);
 	}
