7d5fde851d5f6b520885362534f4764b4cb31ad1
annettaws <anna.damico.ws@gmail.com>
2013-05-23 16:51:18 +0100

    No Clear Script

--- src/bp_implementations/al_bp_ion.c
+++ src/bp_implementations/al_bp_ion.c
@@ -424,9 +424,7 @@ void bp_ion_free_payload(al_bp_bundle_payload_t* payload)
 		free(payload->status_report);
 	}
 	Sdr bpSdr = bp_get_sdr();
-	zco_create_file_ref(bpSdr,
-					"",
-					"");
+	//zco_create_file_ref(bpSdr,"","");
 
 }
 
