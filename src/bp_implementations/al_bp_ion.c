e553c2e314d5f3aaaf65c152e0d247ef3b2e9665
Francesco Apollonio <newsletter_pvt@ldlabs.org>
2014-03-22 11:35:42 +0100

    fixed previous commit (service_num)

--- src/bp_implementations/al_bp_ion.c
+++ src/bp_implementations/al_bp_ion.c
@@ -73,7 +73,12 @@ al_bp_error_t bp_ion_build_local_eid(al_bp_endpoint_id_t* local_eid,
 				if(result == 0)
 					return BP_EBUILDEID;
 			}
-			unsigned long int service_num = getpid()%36000;
+			unsigned long int service_num;
+			if (getpid()<10000)
+				service_num = getpid()+10000;
+			else
+				service_num = getpid();
+
 			printf("AL_BP: pid-> %i - sn-> %lu",getpid(),service_num);
 			sprintf(eidString, "%s:%lu",
 							CBHESCHEMENAME,(unsigned long int)getOwnNodeNbr());
