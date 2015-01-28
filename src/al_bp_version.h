28c1e2a7380f553c6fa79a3beaf9dcffd6d65e18
michirod <michirod@gmail.com>
2015-01-28 13:21:06 +0100

    al_bp version 1.2.2

--- src/al_bp_version.h
+++ src/al_bp_version.h
@@ -14,7 +14,7 @@
 #define AL_BP_VERSION_MAJOR		1
 #define AL_BP_VERSION_MINOR		2
 #define AL_BP_VERSION_PATCH		0
-#define AL_BP_VERSION_STRING	"1.2.1"
+#define AL_BP_VERSION_STRING	"1.2.2"
 
 extern const char* al_bp_version;
 
